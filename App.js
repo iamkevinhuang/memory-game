cec6784750df200895489af91426cbbff0642f5e
nadyasikana <77185193+nadyasikana@users.noreply.github.com>
2021-01-09 19:42:23 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import Board from './components/board';
+import './App.css';
+
+import initializeDeck from './deck'
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [flipped, setFlipped] = useState([]);
+
+  useEffect(() => {
+    setCards(initializeDeck())
+  }, [])
+
+  const handleClick = (id) => {
+    return setFlipped([...flipped, id]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <h2>Bisakah kamu mengingat dimana letak kartu tersebut ?</h2>
+      <Board
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick}
+      />
+    </div>
+  );
+}
+
+export default App;
